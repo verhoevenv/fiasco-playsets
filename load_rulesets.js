e1b3d04aa985e274a7da45deecd075374eae6ac8
Vincent Verhoeven <verhoevenv@gmail.com>
2015-05-20 23:20:08 +0200

    Searchable table
    This involved some parsing of the data, apparently bootstrap tables doesn't search inside arrays so I had to convert them to strings first.

---
+++ load_rulesets.js
@@ -0,0 +1,21 @@
+---
+---
+
+var data = {{site.data.rulesets | jsonify}};
+
+var arrays_to_strings = function(o) {
+	for (var prop in o) {
+		if (o.hasOwnProperty(prop) && $.isArray(o[prop])) {
+			var array = o[prop];
+			o[prop] = array.join(", ");
+		}
+	}
+	return o;
+}
+
+$(function () {
+	data = data.map(arrays_to_strings);
+    $('#table').bootstrapTable({
+        data: data
+    });
+});
